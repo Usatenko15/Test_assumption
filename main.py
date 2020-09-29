523ef5b4808f4852cc6c8be74799ff445adccdaa
Usatenko15 <33806468+Usatenko15@users.noreply.github.com>
2020-09-29 10:43:40 +0300

    ew

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
-import time                          # time package simply writes timestamps for when our server goes up or down.
-from http.server import HTTPServer   # package contains the HTTP server boilerplate from the Python3 standard library.
-from server import Server            # importing a single class, Server from server.py file
+import time
+from http.server import HTTPServer
+from server import Server
 
 # define two constants we’ll be using when we launch the server
 HOST_NAME = 'localhost'
 PORT_NUMBER = 8888
 
 if __name__ == '__main__':
-    httpd = HTTPServer((HOST_NAME, PORT_NUMBER), Server)   # create the HTTP object with previous defined parameters
+    # create the HTTP object with previous defined parameters
+    httpd = HTTPServer((HOST_NAME, PORT_NUMBER), Server)
     print(time.asctime(), 'Server UP - %s:%s' % (HOST_NAME, PORT_NUMBER))
     # this block actually starts up the server and runs it:
     try:
--- server.py
+++ server.py
@@ -1,13 +1,18 @@
-import re  # is used to find words with 6 letters
-import requests  # is used to work with html
-from bs4 import BeautifulSoup  # is used to work with text from html
+import re
+import requests
+from bs4 import BeautifulSoup
 from http.server import BaseHTTPRequestHandler
 
 
-#  This is going to be the class that ours will subclass.
 class Server(BaseHTTPRequestHandler):
-    # When a request comes in, the BaseHTTPRequestHandler will automatically route the request to the
-    # appropriate request method (either do_GET, do_HEAD or do_POST) which we’ve defined on our subclass
+    """"This is going to be the class that ours will subclass.
+    When a request comes in, the BaseHTTPRequestHandler will
+    automatically route the request to the appropriate request method
+    (either do_GET, do_HEAD or do_POST) which we’ve defined on our
+    subclass.
+
+    """
+
     def do_HEAD(self):
         return
 
@@ -17,42 +22,54 @@ class Server(BaseHTTPRequestHandler):
     def do_GET(self):
         self.respond()
 
-    def do_POST(self):
-        return
-
-    # use handle_http to send our basic http handlers and then return the content.
     def handle_http(self, status, content_type):
+        """"Use handle_http to send our basic http handlers and then
+         return the content.
+
+         """
+
         self.send_response(status)
         self.send_header('Content - type', content_type)
         self.end_headers()
         a = 'https://dou.ua' + self.path
-        return bytes(get_text(a), 'UTF-8')
+        return bytes(Server.get_text(a), 'UTF-8')
 
-    #  in charge of sending the actual response out
     def respond(self):
+        """"In charge of sending the actual response out."""
+
         content = self.handle_http(200, 'text / html')
         self.wfile.write(content)
 
+    @staticmethod
+    def get_text(url):
+        """"Method that add a special character if the word consists of 6
+        letters and redirect all links with the domain "dou.ua" to a proxy
+        server.
 
-# method that add a special character if the word consists of 6 letters
-# and redirect all links with the domain "dou.ua" to a proxy server
-def get_text(url):
-    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:80.0) Gecko/20100101 Firefox/80.0'}
-    rs = requests.get(url, headers=headers)
-    root = BeautifulSoup(rs.content, 'html.parser')
-    article = root.find('article')
-    # This for is used to redirect all links with the domain "dou.ua" to a proxy server
-    for a in root.find_all('a'):
-        if a.has_attr('href'):
-            a['href'] = a['href'].replace("https://dou.ua", "http://localhost:8888")
-    # this part of code add a special character if the word consists of 6 letters, using regular expression
-    if article is not None:
-        findtoure = article.find_all(text=re.compile('\s(\w{6})\s|\s(\w{6})\.'))
-        for comment in findtoure:
-            resul = re.findall('\s(\w{6})\s', comment)
-            resul = resul + re.findall('\s(\w{6})\.', comment)
-            fixed_text = comment
-            for kil in resul:
-                fixed_text = re.sub(kil, kil + '™', fixed_text)
-            comment.replace_with(fixed_text)
-    return root.prettify()
+        """
+
+        headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; '
+                                 'rv:80.0) Gecko/20100101 Firefox/80.0'}
+        rs = requests.get(url, headers=headers)
+        root = BeautifulSoup(rs.content, 'html.parser')
+        article = root.find('article')
+        # This for is used to redirect all links with the domain "dou.ua"
+        # to a proxy server
+        for a in root.find_all('a'):
+            if a.has_attr('href'):
+                a['href'] = a['href'].replace("https://dou.ua",
+                                              "http://localhost:8888")
+        # this part of code add a special character if the word consists
+        # of 6 letters, using regular expression
+        if article is not None:
+            findtext = article.find_all(text=re.compile('\s(\w{6})\s|'
+                                                        '\s(\w{6})\.'))
+            for comment in findtext:
+                result = re.findall('\s(\w{6})\s', comment)
+                result = result + re.findall('\s(\w{6})\.', comment)
+                result = list(dict.fromkeys(result))
+                fixed_text = comment
+                for kil in result:
+                    fixed_text = re.sub(kil, kil + '™', fixed_text)
+                comment.replace_with(fixed_text)
+        return root.prettify()
